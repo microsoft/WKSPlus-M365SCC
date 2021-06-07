8f8c6e92dfb535d28b5db2dd9111cc4c504757de
Jorg Van impe <jovanimp@microsoft.com>
2021-06-07 17:28:18 +0200

    update

--- Compliance Center/new-label.ps1
+++ Compliance Center/new-label.ps1
@@ -1,7 +1,7 @@
 ﻿$VerbosePreference = "Continue"
-$LogPath = 'c:\temp'
+$LogPath = "c:\temp"
 Get-ChildItem "$LogPath\*.log" | Where LastWriteTime -LT (Get-Date).AddDays(-15) | Remove-Item -Confirm:$false
-$LogPathName = Join-Path -Path $LogPath -ChildPath "$($MyInvocation.MyCommand.Name)-$(Get-Date -Format 'MM-dd-yyyy').log"
+$LogPathName = Join-Path -Path $LogPath -ChildPath "$($MyInvocation.MyCommand.Name)-$(Get-Date -Format "MM-dd-yyyy").log"
 Start-Transcript $LogPathName -Append
 
 Write-Verbose "$(Get-Date)"
@@ -14,16 +14,16 @@ Write-Verbose "$(Get-Date)"
 
 ###### Connect & Login to ExchangeOnline and Compliance Center (MFA) ######
 $getsessions = Get-PSSession | Select-Object -Property State, Name
-$isconnected = (@($getsessions) -like '@{State=Opened; Name=ExchangeOnlineInternalSession*').Count -gt 0
+$isconnected = (@($getsessions) -like "@{State=Opened; Name=ExchangeOnlineInternalSession*").Count -gt 0
 If ($isconnected -ne "True") {
-    Write-Host -ForegroundColor 'red' 'Will make a connection to Exchange online and Microsoft 365 Compliance Center'
+    Write-Host -ForegroundColor "red" "Will make a connection to Exchange online and Microsoft 365 Compliance Center"
     Start-Sleep -seconds 3
 
 Connect-IPPSSession
 Connect-ExchangeOnline
 }
 else {
-   write-host -ForegroundColor 'Green' " You already have a connection to Office365 compliance Center"
+   write-host -ForegroundColor "Green" " You already have a connection to Office365 compliance Center"
 }
 Start-Sleep -Seconds 5
 
@@ -33,11 +33,11 @@ $Encpermission = $domainname + ":VIEW,VIEWRIGHTSDATA,DOCEDIT,EDIT,PRINT,EXTRACT,
 $groups = (Get-UnifiedGroup).PrimarySmtpAddress
 
 ### Create new label for Highly confidential #######
-New-Label -DisplayName ‘WKS Highly confidential-04’ -Name ‘WKS-Highly-confidential-04’ -ToolTip ‘Contains Highly confidential info’ -Comment ‘Documents with this label contain sensitive data.’ -ContentType "file","Email","Site","UnifiedGroup" -EncryptionEnabled:$true -SiteAndGroupProtectionEnabled:$true -EncryptionPromptUser:$true -EncryptionRightsDefinitions $Encpermission -SiteAndGroupProtectionPrivacy 'private' -SiteExternalSharingControlType 'ExistingExternalUserSharingOnly' -EncryptionDoNotForward:$true -SiteAndGroupProtectionAllowLimitedAccess:$true 
+New-Label -DisplayName "WKS Highly confidential-04" -Name "WKS-Highly-confidential-04" -ToolTip "Contains Highly confidential info" -Comment "Documents with this label contain sensitive data." -ContentType "file","Email","Site","UnifiedGroup" -EncryptionEnabled:$true -SiteAndGroupProtectionEnabled:$true -EncryptionPromptUser:$true -EncryptionRightsDefinitions $Encpermission -SiteAndGroupProtectionPrivacy "private" -SiteExternalSharingControlType "ExistingExternalUserSharingOnly" -EncryptionDoNotForward:$true -SiteAndGroupProtectionAllowLimitedAccess:$true 
 
 Start-Sleep -Seconds 30
 
 ###### publish labels
 
-New-LabelPolicy -name 'WKS-Highly-confidential-publish-03' -Settings @{mandatory=' false'} -AdvancedSettings @{requiredowngradejustification= 'true'} -Labels 'WKS-Highly-confidential-04' 
+New-LabelPolicy -name "WKS-Highly-confidential-publish-03" -Settings @{mandatory=$false} -AdvancedSettings @{requiredowngradejustification= $true} -Labels "WKS-Highly-confidential-04"
 Stop-Transcript
