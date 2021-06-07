3b6726e4b41cfcc9d53ad92eae9f3f2f0db55425
Brendon Lee <brenle@microsoft.com>
2021-06-07 18:20:22 -0400

    added phase/recovery logic and logging function

--- Compliance Center/new-label.ps1
+++ Compliance Center/new-label.ps1
@@ -1,43 +1,200 @@
-﻿$VerbosePreference = "Continue"
-$LogPath = "c:\temp"
-Get-ChildItem "$LogPath\*.log" | Where LastWriteTime -LT (Get-Date).AddDays(-15) | Remove-Item -Confirm:$false
-$LogPathName = Join-Path -Path $LogPath -ChildPath "$($MyInvocation.MyCommand.Name)-$(Get-Date -Format "MM-dd-yyyy").log"
-Start-Transcript $LogPathName -Append
+﻿################ Define Variables ###################
+$LogPath = "c:\temp\"
+$LogCSV = "C:\temp\LabelLog.csv"
+$global:nextPhase = 1
+$global:recovery = $false
 
-Write-Verbose "$(Get-Date)"
+#label
+$labelDisplayName = "WKS Highly Confidential"
+$labelName = "WKS-Highly-Confidential"
+$labelTooltip = "Contains Highly confidential info"
+$labelComment = "Documents with this label contain sensitive data."
 
+#label policy
+$labelPolicyName = "WKS-Highly-confidential-publish"
 
-#### install and load module Exchange online V2 #####
-#Install-Module PowershellGet -Force
-#Install-Module -Name ExchangeOnlineManagement -force
-
-
-###### Connect & Login to ExchangeOnline and Compliance Center (MFA) ######
-$getsessions = Get-PSSession | Select-Object -Property State, Name
-$isconnected = (@($getsessions) -like "@{State=Opened; Name=ExchangeOnlineInternalSession*").Count -gt 0
-If ($isconnected -ne "True") {
-    Write-Host -ForegroundColor "red" "Will make a connection to Exchange online and Microsoft 365 Compliance Center"
-    Start-Sleep -seconds 3
-
-Connect-IPPSSession
-Connect-ExchangeOnline
+################ Functions ###################
+function logWrite([int]$phase, [bool]$result, [string]$logstring)
+{
+    if ($result)
+    {
+        Add-Content -Path $LogCSV -Value "$phase,$result,$(Get-Date),$logString"
+        Write-Host -ForegroundColor Green "$(Get-Date) - Phase $phase : $logstring"
+    } else {
+        Write-Host -ForegroundColor Red "$(Get-Date) - Phase $phase : $logstring"
+    }
 }
-else {
-   write-host -ForegroundColor "Green" " You already have a connection to Office365 compliance Center"
+
+function initialization
+{
+    $pathExists = Test-Path($LogPath)
+
+    if (!$pathExists)
+    {
+        New-Item -ItemType "directory" -Path $LogPath -ErrorAction SilentlyContinue | Out-Null
+    }
+        Add-Content -Path $LogCSV -Value '"Phase","Result","DateTime","Status"'
+        logWrite 0 $true "Initialization completed"
 }
-Start-Sleep -Seconds 5
 
-#### permissions variable #####
-$domainname =  (Get-AcceptedDomain).domainname[0]
-$Encpermission = $domainname + ":VIEW,VIEWRIGHTSDATA,DOCEDIT,EDIT,PRINT,EXTRACT,REPLY,REPLYALL,FORWARD,OBJMODEL"
-$groups = (Get-UnifiedGroup).PrimarySmtpAddress
+function recovery
+{
+    Write-host "Starting recovery..."
+    $global:recovery = $true
+    $savedLog = Import-Csv $LogCSV
+    $lastEntry = (($savedLog.Count) - 1)
+    $lastEntry2 = (($savedLog.Count) - 2)
+    $lastEntryPhase = [int]$savedLog[$lastEntry].Phase
+    $lastEntryResult = $savedLog[$lastEntry].Result
 
-### Create new label for Highly confidential #######
-New-Label -DisplayName "WKS Highly confidential-04" -Name "WKS-Highly-confidential-04" -ToolTip "Contains Highly confidential info" -Comment "Documents with this label contain sensitive data." -ContentType "file","Email","Site","UnifiedGroup" -EncryptionEnabled:$true -SiteAndGroupProtectionEnabled:$true -EncryptionPromptUser:$true -EncryptionRightsDefinitions $Encpermission -SiteAndGroupProtectionPrivacy "private" -SiteExternalSharingControlType "ExistingExternalUserSharingOnly" -EncryptionDoNotForward:$true -SiteAndGroupProtectionAllowLimitedAccess:$true 
+    if ($lastEntryResult -eq $false){
+        if ($lastEntryPhase -eq $savedLog[$lastEntry2].Phase){
+            WriteHost -ForegroundColor Red "The script has failed at Phase $lastEntryPhase repeatedly.  PLease check with your instructor."
+            exit
+        } else {
+            Write-Host "There was a problem with Phase $lastEntryPhase, so trying again...."
+            $global:nextPhase = $lastEntryPhase
+        }
+    } else {
+        # set the phase
+        Write-Host "Phase $lastEntryPhase was successful, so picking up where we left off...."
+        $global:nextPhase = $lastEntryPhase + 1
+    }
+}
 
-Start-Sleep -Seconds 30
+function checkModule 
+{
+    try {
+        Get-Command Connect-ExchangeOnline -ErrorAction Stop | Out-Null
+    } catch {
+        logWrite 1 $false "ExchangeOnlineManagement module is not installed! Exiting."
+        exit
+    }
+    logWrite 1 $True "ExchangeOnlineManagement module is installed."
+    $global:nextPhase++
+}
 
-###### publish labels
+function connectExo
+{
+    try {
+        Get-Command Set-Mailbox -ErrorAction Stop | Out-Null
+    }
+    catch {
+        Write-Host "Connecting to Exchange Online..."
+        Connect-ExchangeOnline
+        try {
+            Get-Command Set-Mailbox -ErrorAction Stop | Out-Null
+        } catch {
+            logWrite 2 $false "Couldn't connect to Exchange Online.  Exiting."
+            exit
+        }
+        if($global:recovery -eq $false){
+            logWrite 2 $true "Successfully connected to Exchange Online"
+            $global:nextPhase++
+        }
+    }
+}
 
-New-LabelPolicy -name "WKS-Highly-confidential-publish-03" -Settings @{mandatory=$false} -AdvancedSettings @{requiredowngradejustification= $true} -Labels "WKS-Highly-confidential-04"
-Stop-Transcript
+function connectSCC
+{
+    try {
+        Get-Command Set-Label -ErrorAction:Stop | Out-Null
+    }
+    catch {
+        Write-Host "Connecting to Compliance Center..."
+        Connect-IPPSSession
+        try {
+            Get-Command Set-Label -ErrorAction:Stop | Out-Null
+        } catch {
+            logWrite 3 $false "Couldn't connect to Compliance Center.  Exiting."
+            exit
+        }
+        if($global:recovery -eq $false){
+            logWrite 3 $true "Successfully connected to Compliance Center"
+            $global:nextPhase++
+        }
+    }
+}
+
+function createLabel
+{
+    <#
+    TO DO:
+    Need to check to see if label exists in case the failure occured after cmd was successful, such as if they close the PS window. Maybe just check if label exists, and use Set-Label if so.
+    #>
+    $domainName = (Get-AcceptedDomain | ?{$_.Default -eq $true}).DomainName
+    $Encpermission = $domainname + ":VIEW,VIEWRIGHTSDATA,DOCEDIT,EDIT,PRINT,EXTRACT,REPLY,REPLYALL,FORWARD,OBJMODEL"
+    Write-Host "Creating label with permissions: $Encpermission..."
+    try {
+        New-Label -DisplayName $labelDisplayName -Name $labelName -ToolTip $labelTooltip -Comment $labelComment -ContentType "file","Email","Site","UnifiedGroup" -EncryptionEnabled:$true -SiteAndGroupProtectionEnabled:$true -EncryptionPromptUser:$true -EncryptionRightsDefinitions $Encpermission -SiteAndGroupProtectionPrivacy "private" -SiteExternalSharingControlType "ExistingExternalUserSharingOnly" -EncryptionDoNotForward:$true -SiteAndGroupProtectionAllowLimitedAccess:$true -ErrorAction Stop | Out-Null
+    } catch {
+        logWrite 4 $false "Error creating label"
+        exit
+    }
+    logWrite 4 $true "Successfully created label"
+    $global:nextPhase++
+    write-host "Sleeping for 30 seconds..."
+    Start-Sleep -Seconds 30
+}
+
+function createPolicy
+{
+        <#
+    TO DO:
+    - Need to check to see if label policy exists in case the failure occured after cmd was successful, such as if they close the PS window. Maybe just check if label exists, and use Set-Label if so.
+    - Need to make sure the labele exists
+    #>
+
+    try {
+        New-LabelPolicy -name $labelPolicyName -Settings @{mandatory=$false} -AdvancedSettings @{requiredowngradejustification= $true} -Labels $labelName -ErrorAction Stop | Out-Null
+    } catch {
+        logWrite 5 $false "Error creating label policy"
+        exit
+    }
+    logWrite 5 $true "Successfully created label policy"
+    $global:nextPhase++
+}
+
+function exitScript
+{
+    Get-PSSession | Remove-PSSession
+    logWrite 6 $true "Session removed successfully."
+}
+
+################ main Script start ###################
+
+if(!(Test-Path($logCSV))){
+    # if log doesn't exist then must be first time we run this, so go to initialization
+    initialization
+} else {
+    # if log already exists, check if we need to recover
+    recovery
+    connectExo
+    connectSCC
+}
+
+#use variable to control phases
+
+if($nextPhase -eq 1){
+checkModule
+}
+
+if($nextPhase -eq 2){
+connectExo
+}
+
+if($nextPhase -eq 3){
+connectSCC
+}
+
+if($nextPhase -eq 4){
+createLabel
+}
+
+if ($nextPhase -eq 5){
+createPolicy
+}
+
+if ($nextPhase -eq 6){
+exitScript
+}
