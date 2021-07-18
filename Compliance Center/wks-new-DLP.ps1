a0aa9df55095f9ee549806789067de5fd8e980fc
Jorg Van impe <jovanimp@microsoft.com>
2021-07-18 08:47:00 +0200

    recoverd DLP script

--- Compliance Center/wks-new-DLP.ps1
+++ Compliance Center/wks-new-DLP.ps1
@@ -1,9 +1,18 @@
 ################ Define Variables ###################
 $LogPath = "c:\temp\"
-$LogCSV = "C:\temp\retentionlog.csv"
+$LogCSV = "C:\temp\LabelLog.csv"
 $global:nextPhase = 1
 $global:recovery = $false
-$global:Sharepoint = ""
+
+#label
+$labelDisplayName = "WKS Highly Confidential"
+$labelName = "WKS-Highly-Confidential"
+$labelTooltip = "Contains Highly confidential info"
+$labelComment = "Documents with this label contain sensitive data."
+
+#label policy
+$labelPolicyName = "WKS-Highly-confidential-publish"
+
 ################ Functions ###################
 function logWrite([int]$phase, [bool]$result, [string]$logstring)
 {
@@ -24,7 +33,7 @@ function initialization
     {
         New-Item -ItemType "directory" -Path $LogPath -ErrorAction SilentlyContinue | Out-Null
     }
-        Add-Content -Path $LogCSV -Value 'Phase,Result,DateTime,Status'
+        Add-Content -Path $LogCSV -Value '"Phase","Result","DateTime","Status"'
         logWrite 0 $true "Initialization completed"
 }
 
@@ -53,7 +62,7 @@ function recovery
     }
 }
 
-function checkModule
+function checkModule 
 {
     try {
         Get-Command Connect-ExchangeOnline -ErrorAction Stop | Out-Null
@@ -64,20 +73,6 @@ function checkModule
     logWrite 1 $True "ExchangeOnlineManagement module is installed."
     $global:nextPhase++
 }
-
-function checkModuleMSOL
-{
-    try {
-        Get-Command Connect-MsolService -ErrorAction Stop | Out-Null
-        
-    } catch {
-        logWrite 2 $false "MSOL module is not installed! Exiting."
-        exit
-    }
-    logWrite 2 $True "MSOL module is installed."
-    $global:nextPhase++
-}
-
 
 function connectExo
 {
@@ -90,11 +85,11 @@ function connectExo
         try {
             Get-Command Set-Mailbox -ErrorAction Stop | Out-Null
         } catch {
-            logWrite 3 $false "Couldn't connect to Exchange Online.  Exiting."
+            logWrite 2 $false "Couldn't connect to Exchange Online.  Exiting."
             exit
         }
         if($global:recovery -eq $false){
-            logWrite 3 $true "Successfully connected to Exchange Online"
+            logWrite 2 $true "Successfully connected to Exchange Online"
             $global:nextPhase++
         }
     }
@@ -111,137 +106,59 @@ function connectSCC
         try {
             Get-Command Set-Label -ErrorAction:Stop | Out-Null
         } catch {
-            logWrite 4 $false "Couldn't connect to Compliance Center.  Exiting."
+            logWrite 3 $false "Couldn't connect to Compliance Center.  Exiting."
             exit
         }
         if($global:recovery -eq $false){
-            logWrite 4 $true "Successfully connected to Compliance Center"
+            logWrite 3 $true "Successfully connected to Compliance Center"
             $global:nextPhase++
         }
     }
 }
 
-function ConnectMsolService
+function createLabel
 {
+    <#
+    TO DO:
+    Need to check to see if label exists in case the failure occured after cmd was successful, such as if they close the PS window. Maybe just check if label exists, and use Set-Label if so.
+    #>
+    $domainName = (Get-AcceptedDomain | ?{$_.Default -eq $true}).DomainName
+    $Encpermission = $domainname + ":VIEW,VIEWRIGHTSDATA,DOCEDIT,EDIT,PRINT,EXTRACT,REPLY,REPLYALL,FORWARD,OBJMODEL"
+    Write-Host "Creating label with permissions: $Encpermission..."
     try {
-        Get-MsolDomain -ErrorAction Stop
-    }
-    catch {
-        Write-Host "Connecting to msol Service..."
-        Connect-MsolService
-        try {
-        Get-MsolContact -ErrorAction Stop
-        } catch {
-            logWrite 5 $false "Couldn't connect to MSOL Service.  Exiting."
-            exit
-        }
-        if($global:recovery -eq $false){
-            logWrite 5 $true "Successfully connected to MSOL Service"
-            $global:nextPhase++
-        }
-    }
-}
-
-Function getdomain
-{
-    try{
-        $InitialDomain = Get-MsolDomain -TenantId $customer.TenantId | Where-Object {$_.IsInitial -eq $true}
-        $global:Sharepoint = "$($InitialDomain.name.split(".")[0])"
-        write-host $global:Sharepoint
-   }catch {
-        logWrite 6 $false "unable to fetch all accepted Domains."
+        New-Label -DisplayName $labelDisplayName -Name $labelName -ToolTip $labelTooltip -Comment $labelComment -ContentType "file","Email","Site","UnifiedGroup" -EncryptionEnabled:$true -SiteAndGroupProtectionEnabled:$true -EncryptionPromptUser:$true -EncryptionRightsDefinitions $Encpermission -SiteAndGroupProtectionPrivacy "private" -SiteExternalSharingControlType "ExistingExternalUserSharingOnly" -EncryptionDoNotForward:$true -SiteAndGroupProtectionAllowLimitedAccess:$true -ErrorAction Stop | Out-Null
+    } catch {
+        logWrite 4 $false "Error creating label"
         exit
     }
-    logWrite 6 $True "Able to get all accepted Domains."
+    logWrite 4 $true "Successfully created label"
     $global:nextPhase++
-
-
+    write-host "Sleeping for 30 seconds..."
+    Start-Sleep -Seconds 30
 }
 
-
-function createSPOSite
+function createPolicy
 {
-    param
-      (
-          [string]$Title  = "wks-compliance-center",
-          [string]$URL = "https://$global:Sharepoint.sharepoint.com/sites/WKS-compliance-center",
-          [string]$Owner = "admin@$global:Sharepoint.onmicrosoft.com",
-          [int]$StorageQuota = "1024",
-          [int]$ResourceQuota = "1024",
-          [string]$Template = "STS#3"
-      )
-   
-  #Connection parameters 
-  $AdminURL = "https://$global:Sharepoint-admin.sharepoint.com"
-   
-  Try{
-      #Connect to Office 365
-      Connect-SPOService -Url $AdminURL
-    
-      #Check if the site collection exists already
-      $SiteExists = Get-SPOSite | where {$_.url -eq $URL}
-      #Check if site exists in the recycle bin
-      $SiteExistsInRecycleBin = Get-SPODeletedSite | where {$_.url -eq $URL}
-   
-      If($SiteExists -ne $null)
-      {
-          write-host "Site $($url) exists already!" -foregroundcolor red
-          
-      }
-      elseIf($SiteExistsInRecycleBin -ne $null)
-      {
-          write-host "Site $($url) exists in the recycle bin!" -foregroundcolor red
-          
-      }
-      else
-      {
-          #sharepoint online create site collection powershell
-          New-SPOSite -Url $URL -title $Title -Owner $Owner -StorageQuota $StorageQuota -NoWait -ResourceQuota $ResourceQuota -Template $Template
-          write-host "Site Collection $($url) Created Successfully!" -foregroundcolor Green
-      }
-  }
-  catch {
-          logWrite 7 $false "Unable to create the SharePoint Website."
-          exit
-      }
-      logWrite 7 $True "Able to create the SharePoint Website."
-      $global:nextPhase++
-}
+        <#
+    TO DO:
+    - Need to check to see if label policy exists in case the failure occured after cmd was successful, such as if they close the PS window. Maybe just check if label exists, and use Set-Label if so.
+    - Need to make sure the labele exists
+    #>
 
-function NewRetentionPolicy
-{
-    Try{
-      
-      #Check if the site collection exists already
-      $rententionExists = Get-RetentionCompliancePolicy -Identity "WKS-Compliance-Retention-SPO-3D"
-              
-      If($rententionExists -ne $null)
-      {
-          write-host "Retention Policy exists already!" -foregroundcolor red
-      }
-      
-      else
-      {
-          #Create compliance retention Policy
-          New-RetentionCompliancePolicy -Name "WKS-Compliance-Retention-SPO-3D-test" -SharePointLocation "https://$global:Sharepoint.sharepoint.com/sites/WKS-compliance-center" -Enabled $true
-          New-RetentionComplianceRule -Name "WKS-Compliance-Retention-SPO-Rule-3D" -Policy "WKS-Compliance-Retention-SPO-3D-test" -RetentionDuration 3
-          write-host "Retention policy and rule are Created Successfully!" -foregroundcolor Green
-      }
-  }
-  catch {
-          logWrite 8 $false "Unable to create the Retention Policy and Rule."
-          exit
-      }
-      logWrite 8 $True "The Retention policy and rule has been created."
-      $global:nextPhase++
+    try {
+        New-LabelPolicy -name $labelPolicyName -Settings @{mandatory=$false} -AdvancedSettings @{requiredowngradejustification= $true} -Labels $labelName -ErrorAction Stop | Out-Null
+    } catch {
+        logWrite 5 $false "Error creating label policy"
+        exit
+    }
+    logWrite 5 $true "Successfully created label policy"
+    $global:nextPhase++
 }
-
 
 function exitScript
 {
     Get-PSSession | Remove-PSSession
-    Disconnect-SPOService
-    logWrite 9 $true "Session removed successfully"
+    logWrite 6 $true "Session removed successfully."
 }
 
 ################ main Script start ###################
@@ -250,59 +167,34 @@ if(!(Test-Path($logCSV))){
     # if log doesn't exist then must be first time we run this, so go to initialization
     initialization
 } else {
-    # if log already exists, check if we need to recover#
+    # if log already exists, check if we need to recover
     recovery
-    checkModule
-    checkModuleMSOL
     connectExo
     connectSCC
-    ConnectMsolService
-    getdomain
-    createSPOSite
-    NewRetentionPolicy
-
-    
-
 }
 
 #use variable to control phases
-
-if($nextPhase -eq 0){
-initialization
-}
 
 if($nextPhase -eq 1){
 checkModule
 }
 
-if($nextPhase -eq2){
-checkModuleMSOL    
-}
-
-if($nextPhase -eq 3){
+if($nextPhase -eq 2){
 connectExo
 }
 
-if($nextPhase -eq 4){
+if($nextPhase -eq 3){
 connectSCC
 }
 
-if($nextPhase -eq 5){
-ConnectMsolService
+if($nextPhase -eq 4){
+createLabel
 }
 
-if($nextPhase -eq 6){
-getdomain
+if ($nextPhase -eq 5){
+createPolicy
 }
 
-if($nextPhase -eq 7){
-    createSPOSite
-}
-
-if ($nextPhase -eq 8){
-    NewRetentionPolicy
-}
-
-if ($nextPhase -eq 9){
+if ($nextPhase -eq 6){
 exitScript
 }
