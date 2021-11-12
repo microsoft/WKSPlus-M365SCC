fa6be74dbaae0d2a97460cfdcf8ce362deb72bfb
mhunecke <33945695+mhunecke@users.noreply.github.com>
2021-11-12 13:46:49 -0300

    Version 2.71

--- Compliance Center/Update-hub.ps1
+++ Compliance Center/Update-hub.ps1
@@ -1,3 +1,3 @@
 New-Item -Path "$env:UserProfile\Desktop\SCLabFiles\Scripts" -ItemType Directory -ErrorAction SilentlyContinue
-Invoke-WebRequest -Uri https://raw.githubusercontent.com/microsoft/WKSPlus-M365SCC/main/Compliance%20Center/download-hub.ps1 -OutFile "$env:UserProfile\Desktop\SCLabFiles\Scripts\download-hub.ps1" -ErrorAction Stop
+Invoke-WebRequest -Uri https://raw.githubusercontent.com/microsoft/WKSPlus-M365SCC/main/Compliance%20Center/Download-hub.ps1 -OutFile "$env:UserProfile\Desktop\SCLabFiles\Scripts\Download-hub.ps1" -ErrorAction Stop
 Set-Location -Path "$env:UserProfile\Desktop\SCLabFiles\Scripts"
