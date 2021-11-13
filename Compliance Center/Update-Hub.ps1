8e38772453a66dfb74de9968cb973ad3a3a42711
mhunecke <33945695+mhunecke@users.noreply.github.com>
2021-11-13 00:25:09 -0300

    Version 2.06

--- Compliance Center/Update-Hub.ps1
+++ Compliance Center/Update-Hub.ps1
@@ -1,6 +1,9 @@
 $LogPath = "$env:UserProfile\Desktop\SCLabFiles\Scripts\"
 Set-Location -Path "$($LogPath)"
 New-Item -Path "$($LogPath)" -ItemType Directory -ErrorAction SilentlyContinue | Out-Null
-Invoke-WebRequest -Uri https://raw.githubusercontent.com/microsoft/WKSPlus-M365SCC/main/Compliance%20Center/Download-Hub.ps1 -OutFile "$($LogPath)Download-Hub.ps1" -ErrorAction Stop
 Remove-Item -Path "$($LogPath)Download-Hub.ps1" -ErrorAction SilentlyContinue | Out-Null
 Remove-Item -Path "$($LogPath)Progress_Download_Log.csv" -ErrorAction SilentlyContinue | Out-Null
+Invoke-WebRequest -Uri https://raw.githubusercontent.com/microsoft/WKSPlus-M365SCC/main/Compliance%20Center/Download-Hub.ps1 -OutFile "$($LogPath)Download-Hub.ps1" -ErrorAction Stop
+
+
+# Invoke-WebRequest -Uri https://raw.githubusercontent.com/microsoft/WKSPlus-M365SCC/main/Compliance%20Center/Update-Hub.ps1 -OutFile "$($LogPath)Update-Hub.ps1" -ErrorAction Stop
