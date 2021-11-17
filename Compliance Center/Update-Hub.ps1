0d992e0693ac2d2f2aa52c9f022016fc3521329d
mhunecke <33945695+mhunecke@users.noreply.github.com>
2021-11-17 01:04:20 -0300

    Version 2.60

--- Compliance Center/Update-Hub.ps1
+++ Compliance Center/Update-Hub.ps1
@@ -4,7 +4,6 @@ New-Item -Path "$($LogPath)" -ItemType Directory -ErrorAction SilentlyContinue |
 Remove-Item -Path "$($LogPath)Download-Hub.ps1" -ErrorAction SilentlyContinue | Out-Null
 Remove-Item -Path "$($LogPath)Progress_Download_Log.csv" -ErrorAction SilentlyContinue | Out-Null
 Invoke-WebRequest -Uri https://raw.githubusercontent.com/microsoft/WKSPlus-M365SCC/main/Compliance%20Center/Download-Hub.ps1 -OutFile "$($LogPath)Download-Hub.ps1" -ErrorAction Stop
-Invoke-WebRequest -Uri https://raw.githubusercontent.com/microsoft/WKSPlus-M365SCC/main/Compliance%20Center/Download-Hub-IRM.ps1 -OutFile "$($LogPath)Download-Hub-IRM.ps1" -ErrorAction Stop
 
 
 # Invoke-WebRequest -Uri https://raw.githubusercontent.com/microsoft/WKSPlus-M365SCC/main/Compliance%20Center/Update-Hub.ps1 -OutFile "$($LogPath)Update-Hub.ps1" -ErrorAction Stop
